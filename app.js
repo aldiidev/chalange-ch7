c9185a7e1c59d696e53d0aa29b7ae0f4cc2adad0
unknown <ahmadmoraldi06@gmail.com>
2023-11-19 21:54:13 +0700

    \init

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const Sentry = require('@sentry/node');
+const { PORT, SENTRY_DSN, SENTRY_ENV} = process.env;
+
+// SENTRY INIT
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0,
+  environment: SENTRY_ENV
+});
+
+//midleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+// SENTRY MIDLEWARE
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+//routes
+app.get('/', (req, res) => {
+  console.log(aldi);
+});
+const authUser = require('./routes/auth.routes');
+app.use('/api/v1/user', authUser);
+
+// SENTRY ERR HANDLER
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+//500
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: false,
+    message: 'internal server error',
+    err: err.message,
+    daata: null,
+  });
+});
+
+app.listen(PORT, () => console.log('app running on port : ', PORT));
